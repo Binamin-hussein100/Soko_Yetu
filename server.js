20fff667cde3216ae9e0fb1b6d2dfe70019db25d
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-02-05 00:09:44 +0300

    db connected

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+// express server
 const app = express();
 const port = 3000
 
 app.use(express.json())
 
+// database connection
+mongoose.connect('mongodb://localhost/SokoSafi')
+    .then(()=>app.listen(3000))
+    .then((err)=>console.log(err))
 app.get("/", (req, res)=>{
     res.send("Trial")
 })
 
 
-app.listen(port, ()=>console.log(`listening on port ${port}`))
+// app.listen(port, ()=>console.log(`listening on port ${port}`))
