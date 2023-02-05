24834eff199d6106e0749aea2f91186213c5434c
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-02-05 11:24:04 +0300

    added create endpoint

---
+++ routes/api/products.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const Product = require('../../models/product');
+const app = express();
+
+app.use(express.json())
+
+// Create a product
+app.post('/products',(req,res)=>{
+    const newProduct = new Product(req.body);
+    newProduct.save((error, product)=>{
+        if(error){
+            return res.status(400).send(error)
+        }
+        res.send(product)
+    })
+})
+
+// // Get all products
+// app.get('/products',(req, res)=>{
+//     Product.find()
+// })
