873985485a9a45d948750bc4c287746009bd49d6
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-02-05 16:37:33 +0300

    Get request for all products

--- routes/api/products.js
+++ routes/api/products.js
@@ -9,13 +9,19 @@ app.post('/products',(req,res)=>{
     const newProduct = new Product(req.body);
     newProduct.save((error, product)=>{
         if(error){
+            console.log("not yet")
             return res.status(400).send(error)
         }
         res.send(product)
     })
 })
 
-// // Get all products
-// app.get('/products',(req, res)=>{
-//     Product.find()
-// })
+// Get all products
+app.get('/products',(req, res)=>{
+    Product.find({}, (error, product)=>{
+        if(error){
+            return res.status(400).send(error)
+        }
+        return res.send(product)
+    })
+})
