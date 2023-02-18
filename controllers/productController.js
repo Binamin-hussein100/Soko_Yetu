5eb91e604560b26aa57743d93e66eaa6428f935a
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-02-18 10:41:10 +0300

    add sample controller/route

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,8 @@ const Product = require('../models/product');
 
 const getProducts = async  (req, res, next)=>{
     const products = await Product.find();
-    res.status(200).json({message: "get alll products"})
+    res.status(200)
+    res.send(products)
 }
 
 module.exports = { getProducts}
