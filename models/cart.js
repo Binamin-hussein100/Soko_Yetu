c818ecbcb64c280d458b0d60caf5bf5bf29ae94c
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2023-02-20 22:47:13 +0300

    feat:add cart entity

---
+++ models/cart.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+const User  = require('./user')
+
+var cartSchema = new mongoose.Schema({
+    user : {
+        type: Schema.Types.ObjectId, ref: User, required: true
+    }
+});
+
+const Cart = mongoose.model('Cart',cartSchema)
+
+module.exports = Cart
